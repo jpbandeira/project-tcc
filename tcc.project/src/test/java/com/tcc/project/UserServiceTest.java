85af61a053576d957e6786e8f7a7795891fb0916
jpbandeira <jpbandeiralima@gmail.com>
2022-04-14 22:51:45 -0300

    adding succesfuly pitest plugin and create reports

--- tcc.project/src/test/java/com/tcc/project/UserServiceTest.java
+++ tcc.project/src/test/java/com/tcc/project/UserServiceTest.java
@@ -1,24 +1,34 @@
 package com.tcc.project;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 import com.tcc.project.enums.TypeUser;
 import com.tcc.project.model.User;
+import com.tcc.project.services.UserService;
 
 public class UserServiceTest {
 	
 	private User user;
+	private UserService userService;
 	
-	@BeforeEach
+	@Before
 	public void before() {
 		this.user = new User(UUID.randomUUID(), "name", TypeUser.STUDENT, "email@email.com", "1710027");
+		this.userService = new UserService();
 	}
 	
 	@Test
 	public void shouldBeAdded() {
+		ArrayList<User> users = new ArrayList<User>();
+		users = this.userService.addUser(user);
 		
+		Assert.assertNotEquals(null, this.user);
+		Assert.assertEquals(this.user, users.get(0));
 	}
+	
 }
