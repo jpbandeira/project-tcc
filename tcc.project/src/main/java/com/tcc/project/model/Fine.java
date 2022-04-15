a4d1cb3aa1c042c282781a5da467a2a9cb9f70a3
jpbandeira <jpbandeiralima@gmail.com>
2022-04-14 23:37:31 -0300

    create base struct models

---
+++ tcc.project/src/main/java/com/tcc/project/model/Fine.java
@@ -0,0 +1,42 @@
+package com.tcc.project.model;
+
+import java.util.UUID;
+
+public class Fine {
+	private UUID uuid;
+	private boolean paid;
+	private User user;
+		
+	public Fine() {}
+
+	public Fine(UUID uuid, boolean paid, User user) {
+		super();
+		this.uuid = uuid;
+		this.paid = paid;
+		this.user = user;
+	}
+
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(UUID uuid) {
+		this.uuid = uuid;
+	}
+
+	public boolean isPaid() {
+		return paid;
+	}
+
+	public void setPaid(boolean paid) {
+		this.paid = paid;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+}
---
+++ tcc.project/src/main/java/com/tcc/project/model/Rent.java
@@ -0,0 +1,44 @@
+package com.tcc.project.model;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Rent {
+	private UUID uuid;
+	private Date dueDate;
+	private User user;
+	
+	public Rent() {
+	}
+
+	public Rent(UUID uuid, Date dueDate, User user) {
+		super();
+		this.uuid = uuid;
+		this.dueDate = dueDate;
+		this.user = user;
+	}
+
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(UUID uuid) {
+		this.uuid = uuid;
+	}
+
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+}
