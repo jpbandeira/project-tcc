654b5404559421336e3840823b68cfb5c634a546
jpbandeira <jpbandeiralima@gmail.com>
2022-05-30 23:28:07 -0300

    adding new methods, tests and reports

--- tcc.project/src/main/java/com/tcc/project/services/RentService.java
+++ tcc.project/src/main/java/com/tcc/project/services/RentService.java
@@ -16,7 +16,43 @@ public class RentService {
 	private UserService userService = new UserService();
 	private BookService bookService = new BookService();
 
-	public ArrayList<Rent> addRent(Rent rent) {
+	public Rent find(UUID uuid) {
+		if (uuid == null) {
+			return null;
+		}
+
+		for (Rent rent : rents) {
+			if (rent.getUuid().equals(uuid)) {
+				return rent;
+			}
+		}
+
+		return null;
+	}
+
+	public ArrayList<Rent> findAllByUser(User user) {
+		ArrayList<Rent> findedRents = new ArrayList<>();
+
+		if (user == null) {
+			return null;
+		}
+
+		if (rents.size() != 0) {
+			for (Rent rent : rents) {
+				if (rent.getUser().equals(user)) {
+					findedRents.add(rent);
+				}
+			}
+		}
+
+		if (findedRents.isEmpty()) {
+			return null;
+		}
+
+		return findedRents;
+	}
+
+	public Rent addRent(Rent rent) {
 		User user = this.userService.findLoged();
 
 		if (user == null) {
@@ -28,8 +64,40 @@ public class RentService {
 		}
 		rent.setUser(user);
 
+		if (!this.checkRentsByUser(rent)) {
+			return null;
+		}
+
 		rents.add(rent);
 
-		return rents;
+		return rent;
+	}
+
+	private Boolean checkRentsByUser(Rent rent) {
+		if (rent.getBook().isRare()) {
+			System.out.println("Cannot rent a rare book!");
+			return false;
+		}
+
+		if (this.checkIfTheBookWasRentedByUser(rent.getUser(), rent.getBook())) {
+			System.out.println("The book " + rent.getBook().getTitle() + " was already rented by " + rent.getUser().getName());
+			return false;
+		}
+
+		return true;
+	}
+
+	private Boolean checkIfTheBookWasRentedByUser(User user, Book book) {
+		ArrayList<Rent> rents = findAllByUser(user);
+
+		if (rents != null) {
+			for (Rent value : rents) {
+				if (book.getUuid().equals(value.getBook().getUuid())) {
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
 }
--- tcc.project/src/test/java/com/tcc/project/RentServiceTest.java
+++ tcc.project/src/test/java/com/tcc/project/RentServiceTest.java
@@ -45,28 +45,31 @@ public class RentServiceTest {
 		BookService.books.clear();
 	}
 
-	private void makeUserBeAuthenticated(TypeUser type) {
+	private User makeUserBeAuthenticated(TypeUser type) {
 		for (User user : this.userService.findAll()) {
 			if (user.getType().equals(type)) {
 				user.setAuthenticated(true);
+				return user;
 			}
 		}
+
+		return null;
 	}
 
 	@Test
 	public void rentShouldNotBeAddedWithoutUserRegistered() {
 		this.cleanList();
 
-		ArrayList<Rent> rents = this.rentService.addRent(new Rent());
-		Assert.assertNull(rents);
+		Rent rent = this.rentService.addRent(new Rent());
+		Assert.assertNull(rent);
 	}
 
 	@Test
 	public void rentShouldNotBeAddedWithNull() {
 		this.makeUserBeAuthenticated(TypeUser.STUDENT);
 
-		ArrayList<Rent> rents = this.rentService.addRent(null);
-		Assert.assertNull(rents);
+		Rent rent = this.rentService.addRent(null);
+		Assert.assertNull(rent);
 
 		this.cleanList();
 	}
@@ -79,8 +82,9 @@ public class RentServiceTest {
 		this.rentService.addRent(setUp());
 		this.rentService.addRent(setUp());
 		this.rentService.addRent(setUp());
-		ArrayList<Rent> rents = this.rentService.addRent(setUp());
-		Assert.assertNotNull(rents);
+		Rent rent= this.rentService.addRent(setUp());
+		Assert.assertNotNull(rent);
+		Assert.assertNotNull(rent.getUser());
 
 		this.cleanList();
 	}
@@ -95,7 +99,104 @@ public class RentServiceTest {
 		this.rentService.addRent(setUp());
 		this.rentService.addRent(setUp());
 		this.rentService.addRent(setUp());
-		ArrayList<Rent> rents = this.rentService.addRent(setUp());
+		Rent rent = this.rentService.addRent(setUp());
+		Assert.assertNotNull(rent);
+		Assert.assertNotNull(rent.getUser());
+
+		this.cleanList();
+	}
+
+	@Test
+	public void rentShouldNotBeAddedWithARareBook() {
+		this.makeUserBeAuthenticated(TypeUser.STUDENT);
+
+		Rent rentSetup = this.setUp();
+		rentSetup.getBook().setRare(true);
+
+		Rent rent = this.rentService.addRent(rentSetup);
+		Assert.assertNull(rent);
+
+		this.cleanList();
+	}
+
+	@Test
+	public void rentShouldNotBeAddedWhenAUserAlreadyDidThis() {
+		this.makeUserBeAuthenticated(TypeUser.STUDENT);
+
+		Rent rent1 = setUp();
+		this.rentService.addRent(rent1);
+		Rent rent2 = setUp();
+		rent2.setBook(rent1.getBook());
+		Rent rent = this.rentService.addRent(rent2);
+
+		Assert.assertNull(rent);
+		this.cleanList();
+	}
+
+	@Test
+	public void shouldNotFindARentWithANullUUID() {
+		Rent rent = this.rentService.find(null);
+		Assert.assertNull(rent);
+
+		this.cleanList();
+	}
+
+	@Test
+	public void shouldNotFindARentWithAInvalidUUID() {
+		this.makeUserBeAuthenticated(TypeUser.STUDENT);
+
+		this.rentService.addRent(setUp());
+		this.rentService.addRent(setUp());
+
+		Rent rent = this.rentService.find(UUID.randomUUID());
+		Assert.assertNull(rent);
+
+		this.cleanList();
+	}
+
+	@Test
+	public void shouldFindARent() {
+		this.makeUserBeAuthenticated(TypeUser.STUDENT);
+
+		Rent rent = setUp();
+		this.rentService.addRent(rent);
+		this.rentService.addRent(setUp());
+
+		Rent value = this.rentService.find(rent.getUuid());
+		Assert.assertNotNull(value);
+
+		this.cleanList();
+	}
+
+	@Test
+	public void shouldNotFindAllByUserNullUser() {
+		this.cleanList();
+		//User user = this.makeUserBeAuthenticated(TypeUser.STUDENT);
+
+		ArrayList<Rent> rents = this.rentService.findAllByUser(null);
+
+		Assert.assertNull(rents);
+	}
+
+	@Test
+	public void shouldNotFindAllByUserWithoutRents() {
+		User user = this.makeUserBeAuthenticated(TypeUser.STUDENT);
+
+		ArrayList<Rent> rents = this.rentService.findAllByUser(user);
+		Assert.assertNull(rents);
+
+		this.cleanList();
+	}
+
+	@Test
+	public void shouldFindAllByUser() {
+		User user = this.makeUserBeAuthenticated(TypeUser.STUDENT);
+
+		this.rentService.addRent(setUp());
+		this.rentService.addRent(setUp());
+		this.rentService.addRent(setUp());
+
+		ArrayList<Rent> rents = this.rentService.findAllByUser(user);
 		Assert.assertNotNull(rents);
 
 		this.cleanList();
